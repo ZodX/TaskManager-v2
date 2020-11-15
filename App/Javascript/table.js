073de481fc22cb2b38b1223a9fd80dcf68ec8521
Molnár András Imre <m.andras.imre00@gmail.com>
2020-11-15 23:47:25 +0100

    Database ID set implemented

--- App/Javascript/table.js
+++ App/Javascript/table.js
@@ -1,1 +1,13 @@
 let db = new Localbase('db')
+
+var all_id = [];
+db.collection('tasks').get().then(tasks => {
+    tasks.forEach(task => {
+        all_id.push(task.id);
+    });
+}).then(function() {
+        if (all_id.length > 0) {
+            current_id = Math.max(...all_id) + 1;
+        } else {
+            current_id = 1;
+    }});
