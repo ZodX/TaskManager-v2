3e08aa2e16b1987f926cb1a4725646a01582e743
Molnár András Imre <m.andras.imre00@gmail.com>
2020-11-22 15:39:19 +0100

    HTML redirect to list selected group's elements

--- App/Javascript/table.js
+++ App/Javascript/table.js
@@ -1,39 +1,40 @@
 let db = new Localbase('db')
 
+
 var all_id = [];
 db.collection('tasks').get().then(tasks => {
     tasks.forEach(task => {
         all_id.push(task.id);
     });
 }).then(function() {
-        if (all_id.length > 0) {
-            current_id = Math.max(...all_id) + 1;
-        } else {
+    if (all_id.length > 0) {
+        current_id = Math.max(...all_id) + 1;
+    } else {
             current_id = 1;
-    }})
-    .then(function() {
-        getAllBtn();
-    });
-
-let buttonAdd = document.getElementById("addBtn")
-buttonAdd.addEventListener('click', addBtn)
-
-function getCurrentDate() {
-    var currentdate = new Date(); 
-    var datetime =  currentdate.getDate() + "/"
-                    + (currentdate.getMonth()+1)  + "/" 
-                    + currentdate.getFullYear();
-
-    return datetime;
-}
-
-function addBtn() {
-    var task_name = document.felvesz.addInp1.value;
-    var task_description = document.felvesz.addInp2.value;
-    var task_priority = document.felvesz.addInp3.value;
-
-    db.collection('tasks').add({
-        id: current_id++,
+        }})
+        .then(function() {
+            getAllBtn();
+        });
+        
+        let buttonAdd = document.getElementById("addBtn")
+        buttonAdd.addEventListener('click', addBtn)
+        
+        function getCurrentDate() {
+            var currentdate = new Date(); 
+            var datetime =  currentdate.getDate() + "/"
+            + (currentdate.getMonth()+1)  + "/" 
+            + currentdate.getFullYear();
+            
+            return datetime;
+        }
+        
+        function addBtn() {
+            var task_name = document.felvesz.addInp1.value;
+            var task_description = document.felvesz.addInp2.value;
+            var task_priority = document.felvesz.addInp3.value;
+            
+            db.collection('tasks').add({
+                id: current_id++,
         name: task_name,
         group: task_description,
         priority: task_priority,
@@ -54,37 +55,78 @@ function getAllBtn() {
     var container = document.getElementById("tasksContainer");
     container.innerHTML = ``;
 
+    try {
+        group_to_list = sessionStorage.getItem('clicked_group');
+    } catch (error) {
+
+    }
+    
+    console.log(group_to_list);
+
     db.collection('tasks').orderBy('id').get()
-        .then(tasks => {
-            console.log('tasks: ', tasks)
-            tasks.forEach(task => {
+    .then(tasks => {
+        console.log('tasks: ', tasks)
+        tasks.forEach(task => {
+            if (group_to_list == null) {
                 if (!task.completed) {
                     container.innerHTML += `
                     <div class="taskTabletDesktop">
-                        <p class="col-4">${task.name}</p>
-                        <p class="col-2">${task.group}</p>
-                        <p class="col-1">${task.priority}</p>
-                        <p class="col-2">${task.date}</p>
-                        <p class="col-1"><button onclick="doneBtnPressed(this.id)" id="F${task.id}" class="btn btn-success">Done</button></p>
-                        <p class="col-1"><button onclick="updateBtnPressed(this.id)" id="U${task.id}" class="btn btn-warning">Modify</button></p>
-                        <p class="col-1"><button onclick="deleteBtnPressed(this.id)" id="D${task.id}" class="btn btn-danger">Delete</button></p>
-                    </div>
-
-                    <div class="taskMobile">
-                        <p class="taskMobileLabel"><b>${task.name}</b></p>
-                        <p class="taskMobileLabel taskMobileDescription">${task.group}</p>
-                        <p class="taskMobileLabel">Priority: ${task.priority}</p>
-                        <p class="taskMobileLabel">Last Modified: <i>${task.date}</i></p>
-                        <div class="mobileBtnContainer">
-                            <button onclick="doneBtnPressed(this.id)" id="F${task.id}" class="btn btn-success">Done</button>
-                            <button onclick="updateBtnPressed(this.id)" id="U${task.id}" class="btn btn-warning">Modify</button>
-                            <button onclick="deleteBtnPressed(this.id)" id="D${task.id}" class="btn btn-danger">Delete</button>
+                    <p class="col-4">${task.name}</p>
+                    <p class="col-2">${task.group}</p>
+                    <p class="col-1">${task.priority}</p>
+                    <p class="col-2">${task.date}</p>
+                            <p class="col-1"><button onclick="doneBtnPressed(this.id)" id="F${task.id}" class="btn btn-success">Done</button></p>
+                            <p class="col-1"><button onclick="updateBtnPressed(this.id)" id="U${task.id}" class="btn btn-warning">Modify</button></p>
+                            <p class="col-1"><button onclick="deleteBtnPressed(this.id)" id="D${task.id}" class="btn btn-danger">Delete</button></p>
                         </div>
-                    </div>
-                    `;   
+        
+                        <div class="taskMobile">
+                            <p class="taskMobileLabel"><b>${task.name}</b></p>
+                            <p class="taskMobileLabel taskMobileDescription">${task.group}</p>
+                            <p class="taskMobileLabel">Priority: ${task.priority}</p>
+                            <p class="taskMobileLabel">Last Modified: <i>${task.date}</i></p>
+                            <div class="mobileBtnContainer">
+                                <button onclick="doneBtnPressed(this.id)" id="F${task.id}" class="btn btn-success">Done</button>
+                                <button onclick="updateBtnPressed(this.id)" id="U${task.id}" class="btn btn-warning">Modify</button>
+                                <button onclick="deleteBtnPressed(this.id)" id="D${task.id}" class="btn btn-danger">Delete</button>
+                            </div>
+                        </div>
+                        `;   
+                }  
+            } else {
+                if (task.group == group_to_list) {
+                    container.innerHTML += `
+                    <div class="taskTabletDesktop">
+                    <p class="col-4">${task.name}</p>
+                    <p class="col-2">${task.group}</p>
+                    <p class="col-1">${task.priority}</p>
+                    <p class="col-2">${task.date}</p>
+                            <p class="col-1"><button onclick="doneBtnPressed(this.id)" id="F${task.id}" class="btn btn-success">Done</button></p>
+                            <p class="col-1"><button onclick="updateBtnPressed(this.id)" id="U${task.id}" class="btn btn-warning">Modify</button></p>
+                            <p class="col-1"><button onclick="deleteBtnPressed(this.id)" id="D${task.id}" class="btn btn-danger">Delete</button></p>
+                        </div>
+        
+                        <div class="taskMobile">
+                            <p class="taskMobileLabel"><b>${task.name}</b></p>
+                            <p class="taskMobileLabel taskMobileDescription">${task.group}</p>
+                            <p class="taskMobileLabel">Priority: ${task.priority}</p>
+                            <p class="taskMobileLabel">Last Modified: <i>${task.date}</i></p>
+                            <div class="mobileBtnContainer">
+                                <button onclick="doneBtnPressed(this.id)" id="F${task.id}" class="btn btn-success">Done</button>
+                                <button onclick="updateBtnPressed(this.id)" id="U${task.id}" class="btn btn-warning">Modify</button>
+                                <button onclick="deleteBtnPressed(this.id)" id="D${task.id}" class="btn btn-danger">Delete</button>
+                            </div>
+                        </div>
+                        `;   
                 }
+            }
             });
         })
+
+        try {
+            sessionStorage.removeItem('clicked_group');
+        } catch (error) {
+        }
 }
 
 function deleteBtnPressed(clicked_id) {
