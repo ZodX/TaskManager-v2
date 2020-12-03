3f772eb343b2709cf46f46f3576a33de8a9240f1
Bacsik Mátyás <bacsik.matyas@gmail.com>
2020-12-03 17:36:46 +0100

    table.html loading if necessary added

--- App/Javascript/groups.js
+++ App/Javascript/groups.js
@@ -26,110 +26,117 @@ function getAllGroupsOnCards() {
         number_of_groups = groups.size;
         number_of_rows = number_of_groups / 3;
     }).then(function () {
-        count = 0;
         
-        if (number_of_rows > 0) {
-            container.innerHTML += `
-                    <div class="row rowTabletDesktop">
-                    </div>
-                    `;
-            var currentRow = document.querySelector(".row:last-child");
-        }
-
-        db.collection('tasks').orderBy('id').get()
-        .then(tasks => {
-            console.log('tasks: ', tasks);
-            groups.forEach(group => {
-                if (count < 3) {
-                    currentRow.innerHTML += `
-                    <div class="vercitalPh"></div>
-                        <div class="groupCard">
-                            <div class="groupElementContainer groupCardTitleContainer">
-                                <p class="groupCardTitle">${group}</p>
-                            </div>
-                            <div class="groupElementContainer groupCardCountContainer">
-                                <p class="groupElementsCount">${groupCounts.get(group)}</p>
-                            </div>
-                            <div class="groupElementContainer groupElementCountTextContainer">
-                                <p class="groupElementCountText">(Number of tasks)</p>
-                            </div>
-                            <div class="groupElementContainer groupCardBtnContainer">
-                                <button id="${group}" onclick="listTasksPressed(this.id)" class="btn btn-success">List Tasks</button>
-                            </div>
-                        </div>
-                    `;
-                    count++;
-                } else {
-                    count = 1;
-                    container.innerHTML += `
-                    <div class="row rowTabletDesktop">
-                        <div class="vercitalPh"></div>
-                        <div class="groupCard">
-                            <div class="groupElementContainer groupCardTitleContainer">
-                                <p class="groupCardTitle">${group}</p>
-                            </div>
-                            <div class="groupElementContainer groupCardCountContainer">
-                                <p class="groupElementsCount">${groupCounts.get(group)}</p>
-                            </div>
-                            <div class="groupElementContainer groupElementCountTextContainer">
-                                <p class="groupElementCountText">(Number of tasks)</p>
-                            </div>
-                            <div class="groupElementContainer groupCardBtnContainer">
-                                <button id="${group}" onclick="listTasksPressed(this.id)" class="btn btn-success">List Tasks</button>
-                            </div>
-                        </div>
-                    </div>
-                    `;
-                    currentRow = document.querySelector(".row:last-child");
-                }
-            });
-        }).then(function () {
-            if (number_of_groups % 3 == 0) {
+        if(groups.size>1){
+        
+            count = 0;
+            
+            if (number_of_rows > 0) {
                 container.innerHTML += `
-                    <div class="row">
-                    </div>
-                    `;
-                currentRow = document.querySelector(".row:last-child");
-                currentRow.innerHTML += `
-                <div class="vercitalPh"></div>
-                <div class="plusCard">
-                    <img onclick="newGroupBtnPressed()"/>
-                </div>
-                `;
-                console.log("tasknumber: " + number_of_groups)
-                console.log(number_of_groups % 3)
-            } else {
-                currentRow.innerHTML += `
-                <div class="vercitalPh"></div>
-                <div class="plusCard">
-                    <img onclick="newGroupBtnPressed()"/>
-                </div>
-                `;
+                        <div class="row rowTabletDesktop">
+                        </div>
+                        `;
+                var currentRow = document.querySelector(".row:last-child");
             }
 
+            db.collection('tasks').orderBy('id').get()
+            .then(tasks => {
+                console.log('tasks: ', tasks);
+                groups.forEach(group => {
+                    if (count < 3) {
+                        currentRow.innerHTML += `
+                        <div class="vercitalPh"></div>
+                            <div class="groupCard">
+                                <div class="groupElementContainer groupCardTitleContainer">
+                                    <p class="groupCardTitle">${group}</p>
+                                </div>
+                                <div class="groupElementContainer groupCardCountContainer">
+                                    <p class="groupElementsCount">${groupCounts.get(group)}</p>
+                                </div>
+                                <div class="groupElementContainer groupElementCountTextContainer">
+                                    <p class="groupElementCountText">(Number of tasks)</p>
+                                </div>
+                                <div class="groupElementContainer groupCardBtnContainer">
+                                    <button id="${group}" onclick="listTasksPressed(this.id)" class="btn btn-success">List Tasks</button>
+                                </div>
+                            </div>
+                        `;
+                        count++;
+                    } else {
+                        count = 1;
+                        container.innerHTML += `
+                        <div class="row rowTabletDesktop">
+                            <div class="vercitalPh"></div>
+                            <div class="groupCard">
+                                <div class="groupElementContainer groupCardTitleContainer">
+                                    <p class="groupCardTitle">${group}</p>
+                                </div>
+                                <div class="groupElementContainer groupCardCountContainer">
+                                    <p class="groupElementsCount">${groupCounts.get(group)}</p>
+                                </div>
+                                <div class="groupElementContainer groupElementCountTextContainer">
+                                    <p class="groupElementCountText">(Number of tasks)</p>
+                                </div>
+                                <div class="groupElementContainer groupCardBtnContainer">
+                                    <button id="${group}" onclick="listTasksPressed(this.id)" class="btn btn-success">List Tasks</button>
+                                </div>
+                            </div>
+                        </div>
+                        `;
+                        currentRow = document.querySelector(".row:last-child");
+                    }
+                });
+            }).then(function () {
+                if (number_of_groups % 3 == 0) {
+                    container.innerHTML += `
+                        <div class="row">
+                        </div>
+                        `;
+                    currentRow = document.querySelector(".row:last-child");
+                    currentRow.innerHTML += `
+                    <div class="vercitalPh"></div>
+                    <div class="plusCard">
+                        <img onclick="newGroupBtnPressed()"/>
+                    </div>
+                    `;
+                    console.log("tasknumber: " + number_of_groups)
+                    console.log(number_of_groups % 3)
+                } else {
+                    currentRow.innerHTML += `
+                    <div class="vercitalPh"></div>
+                    <div class="plusCard">
+                        <img onclick="newGroupBtnPressed()"/>
+                    </div>
+                    `;
+                }
 
-            groups.forEach(group => {
-                container.innerHTML += `
-                <div class="taskMobile">
-                    <div class="groupCardMobile">
-                        <div class="groupMobileContainer groupMobileTitle">
-                            <p class="groupMobileLabel"><b>${group}</b></p>
-                        </div>
-                        <div class="groupMobileContainer groupMobileCount">
-                            <p class="groupMobileLabel">${groupCounts.get(group)}</p>
-                        </div>
-                        <div class="groupMobileContainer groupMobileDescription">
-                            <p class="groupMobileLabel">(Number of tasks)</p>
-                        </div>
-                        <div class="groupMobileContainer groupMobileBtn">
-                            <button class="btn btn-success">List Tasks</button>
+
+                groups.forEach(group => {
+                    container.innerHTML += `
+                    <div class="taskMobile">
+                        <div class="groupCardMobile">
+                            <div class="groupMobileContainer groupMobileTitle">
+                                <p class="groupMobileLabel"><b>${group}</b></p>
+                            </div>
+                            <div class="groupMobileContainer groupMobileCount">
+                                <p class="groupMobileLabel">${groupCounts.get(group)}</p>
+                            </div>
+                            <div class="groupMobileContainer groupMobileDescription">
+                                <p class="groupMobileLabel">(Number of tasks)</p>
+                            </div>
+                            <div class="groupMobileContainer groupMobileBtn">
+                                <button class="btn btn-success">List Tasks</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                `;
-            })       
-        })
-    })    
+                    `;
+                })       
+            })
+        }
+        else{
+            window.location.replace("../Pages/table.html");
+        }
+    })//    
 }
 
 function listTasksPressed(clicked_group) {
